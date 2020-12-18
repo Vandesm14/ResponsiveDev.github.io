95e1ebfe3881c28552276d941b6987f8c04ca704
vandesm14 <vandesm14@gmail.com>
2020-12-17 21:39:25 -0500

    add some components

---
+++ src/Home.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Header from './comps/Header.svelte';
+
+	let header = {
+		title: 'Welcome to Responsive!',
+		description: `Two guys talk with the community about all things tech; from frameworks to tech pop-tarts, we've got you covered.`
+	};
+</script>
+
+<Header {...header}/>
---
+++ src/comps/Header.svelte
@@ -0,0 +1,22 @@
+<script>
+	export let title = '', 
+	description = '',
+	button = '',
+	href = '';
+</script>
+
+<style>
+	.header {
+		display: flex;
+		text-align: center;
+		background-color: blue;
+	}
+</style>
+
+<div class="header">
+	<h1>{title}</h1>
+	<p>{description}</p>
+	{#if button}
+	<a href="{href}">{button}</a>
+	{/if}
+</div>
