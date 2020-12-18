137eea709fdc114fba721bdea2e531950caafb3a
vandesm14 <vandesm14@gmail.com>
2020-12-17 23:06:16 -0500

    update home page

--- src/Home.svelte
+++ src/Home.svelte
@@ -1,10 +1,22 @@
 <script>
 	import Header from './comps/Header.svelte';
+	import Article from './comps/Article.svelte';
 
 	let header = {
 		title: 'Welcome to Responsive!',
-		description: `Two guys talk with the community about all things tech; from frameworks to tech pop-tarts, we've got you covered.`
+		button: 'Join Us',
+		href: ''
+	};
+	let about = {
+		title: 'About Us',
+		button: 'Join Us',
+		href: ''
 	};
 </script>
 
-<Header {...header}/>
+<Header {...header}>
+	Two guys talk with the community about all things tech; from frameworks to tech pop-tarts, we've got you covered.
+</Header>
+<Article {...about}>
+	Hello World
+</Article>
--- src/comps/Article.svelte
+++ src/comps/Article.svelte
@@ -8,14 +8,12 @@
 <style>
 div {
   display: flex;
-  align-items: center;
+  align-items: right;
   flex-direction: column;
   justify-content: center;
   padding: 0 10vw;
   height: 70vh;
-  background-color: #5a507e;
-  color: white;
-  text-align: center;
+  color: black;
   font-size: 1.6rem;
 }
 
@@ -23,22 +21,32 @@ div > * {
   margin: 30px 0;
 }
 
+div > *:empty {
+	display: none;
+}
+
+h1 {
+	margin-bottom: 0;
+}
+
 a {
+  margin-right: auto;
+  margin-left: auto;
   padding: 10px 20px;
   width: max-content;
   border-radius: 1000px;
-  background-color: white;
-  color: #5a507e;
+  background-color: #5a507e;
+  color: white;
   text-decoration: none;
   transition: 150ms ease-out;
 }
 a:hover {
-  box-shadow: 0 4px 20px #00000044;
+  box-shadow: 0 4px 16px #00000033;
   transform: scale(1.1);
 }
 </style>
 
-<div class="header">
+<div class="article">
 	<h1>{title}</h1>
 	{#if description}
 	<p>{description}</p>
